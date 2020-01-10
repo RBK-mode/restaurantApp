2d434ce274ea1639fce12c242e0899659bd11d0d
Amera Ayad <amera.ayad.it@gmail.com>
2020-01-10 19:17:26 +0100

    add order model and routers

---
+++ src/models/Order.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const orderSchema = mongoose.Schema({
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Customer'
+    },
+    items_list: [{
+        _id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        img: {
+            type: String,
+            required: true
+        },
+        categoryId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true
+        },
+        quantity: {
+            type: Number,
+            default: 1
+        }
+    }],
+    location: {
+        latitude: Number,
+        longitude: Number
+    },
+    createdAt: {
+        type: Number,
+        default: Date.now()
+    },
+    deliveredAt: {
+        type: Number
+    },
+    state: {
+        type: String,
+        default: 'requested'
+    }
+})
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
+
