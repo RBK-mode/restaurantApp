bf8c7de49d746ae93b77f79830bdc6bff667e49b
FoozBarakat <reachfooz@gmail.com>
2020-01-11 10:51:35 +0100

    addToMenu

--- client/src/components/ItemItem.js
+++ client/src/components/ItemItem.js
@@ -6,7 +6,7 @@ export default function ItemItem(props) {
       <h2>{props.item.name}</h2>
       <img src={props.item.img} />
       <h3>Price: ${props.item.price}</h3>
-      <button onClick={() => props.addToMenu(props.item)}> Add To Menu </button>
+      <button onClick={() => props.addToMenu(props.item._id)}> Add To Menu </button>
       <h3>Category: {props.item.categoryId.name}</h3>
 
     </div>
--- src/routers/menu.js
+++ src/routers/menu.js
@@ -5,7 +5,7 @@ const Category = require('../models/category');
 
 const router = express.Router();
 
-router.get('/', auth, async(req, res) => {
+router.get('/', auth, async (req, res) => {
     try {
         const doc = await Menu.find().populate('itemId').exec();
         res.status(200).json(doc);
@@ -15,12 +15,14 @@ router.get('/', auth, async(req, res) => {
     }
 });
 
-router.post('/', auth, async(req, res) => {
+router.post('/', auth, async (req, res) => {
     try {
         const { itemId } = req.body;
-        var menu = new Menu({ itemId });
+        console.log(itemId)
+        var menu = new Menu({ itemId: itemId });
         const doc = await menu.save();
-        res.status(201).json(doc);
+        const doc2 = await Menu.findById({ _id: itemId }).populate('itemId').exec();
+        res.status(201).json(doc2);
     } catch (err) {
         console.log(err)
         res.status(400).send();
@@ -28,7 +30,7 @@ router.post('/', auth, async(req, res) => {
 
 });
 
-router.delete('/delete/:id', auth, async(req, res) => {
+router.delete('/delete/:id', auth, async (req, res) => {
     try {
         await Menu.findByIdAndDelete(req.params.id);
         res.status(200).json('removed');
